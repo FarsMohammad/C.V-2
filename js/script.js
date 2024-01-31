9fd8f030c8243be0ce08a6a98dbe12a3a07d6154
FarsMohammad <145973398+FarsMohammad@users.noreply.github.com>
2024-01-31 14:40:48 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ========== tying animation ==========*/ 
+var typed = new Typed(".typing",{
+    strings:["","Web Developer.","Web Designer","A passionate programmer."],
+    typeSpeed:100,
+    backSpeed:65,
+    loop:true
+})
